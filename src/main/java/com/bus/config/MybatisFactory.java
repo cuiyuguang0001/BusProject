392a22f8ad60595b6dc693d320659efe2f35436b
cuiyuguang <651826284@qq.com>
2019-11-15 14:57:03 +0800

    cui

---
+++ src/main/java/com/bus/config/MybatisFactory.java
@@ -0,0 +1,12 @@
+package com.bus.config;
+
+import com.person.factory.core.FactoryApplication;
+
+//数据库实体类生成器
+public class MybatisFactory {
+
+    public static void main(String[] args) {
+        FactoryApplication.run("mysql", "com.mysql.jdbc.Driver", "root", "x5", "jdbc:mysql://localhost:3306/zyq?useSSL=false&serverTimezone=UTC", "com.bus", "zyq");
+    }
+
+}
