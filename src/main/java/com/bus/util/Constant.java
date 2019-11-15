003377f490bda7a1e47de9b44d8de00d9aff1df8
cuiyuguang <651826284@qq.com>
2019-11-15 15:22:20 +0800

    cui

---
+++ src/main/java/com/bus/util/Constant.java
@@ -0,0 +1,8 @@
+package com.bus.util;
+
+public class Constant {
+
+    public static final int REQUEST = 200;
+    public static final int ERROR = 500;
+
+}
---
+++ src/main/java/com/bus/util/Request.java
@@ -0,0 +1,29 @@
+package com.bus.util;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Request extends HashMap<String, Object> {
+
+    public Request(){
+        put("code", 0);
+        put("ref", Constant.REQUEST);
+    }
+
+    public Request ok(String msg){
+        put("msg", msg);
+        return this;
+    }
+
+    public Request okList(List list, int count){
+        put("data", list);
+        put("data", count);
+        return this;
+    }
+
+    public Request error(String msg){
+        put("msg", msg);
+        put("ref", Constant.ERROR);
+    }
+
+}
